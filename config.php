8b875d7a37fece151b9174c61146828bfb1e76a8
Matthew Wilson <wormsetc@gmail.com>
2015-03-02 13:26:06 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+  $base_url = "/message_board";
+  $siteTitle = "Sitting Spots";
+  $slug = "Need a place to sit?";
+  $link = mysqli_connect('localhost', 'wormse6_messboar', 'se4DR%ft6', 'wormse6_oct2014mb');
+  if (!$link) {
+    die('Could not connect: ' . mysql_error());
+  }
+  $main_links = 
+    '<li><a href="index.php">Home</a></li>
+    <li><a href="find_place.php">Find a place</a></li>
+    <li><a href="submit_a_place.php">Tell us your place</a></li>';
+?>
